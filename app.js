02397f264fc5b7f6b6c4858f6e3d31313e25b10e
Targarius <theshark158@gmail.com>
2021-06-07 12:30:36 +0200

    adding js left

---
+++ app.js
@@ -0,0 +1,34 @@
+let result = document.querySelector("#screen");
+
+let currentResult = 0;
+let operation = 0;
+
+console.log(result.innerHTML);
+
+const add = (a) => {
+    result.innerHTML += a;
+}
+
+const addition = (a) => {
+
+}
+
+const soustraction = (a) => {
+    
+}
+
+const division = (a) => {
+    
+}
+
+const multiplication = (a) => {
+    
+}
+
+const del = () => {
+    
+}
+
+const equal = () => {
+    
+}
